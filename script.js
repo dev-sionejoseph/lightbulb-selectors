b70c9c85905d602b82fc6b4fc6c257e6f9786215
Sione <dev.sionejoseph@gmail.com>
2024-09-04 18:58:32 -0500

    linked script and selected lightbulbs

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 // Write your code here
+console.log("script connected successfully!")
+
+let lightbulb1= document.querySelector('#lightbulb1');
+let lightbulb2= document.querySelector('#lightbulb2');
+let lightbulb3= document.querySelector('#lightbulb3');
