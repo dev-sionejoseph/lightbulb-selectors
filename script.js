c29e7e6ad98546a42b065c6c9913951de27a7317
Sione <dev.sionejoseph@gmail.com>
2024-09-04 20:21:30 -0500

    done with required tasks

--- script.js
+++ script.js
@@ -5,9 +5,21 @@ let lightbulb1= document.querySelector('#lightbulb1');
 let lightbulb2= document.querySelector('#lightbulb2');
 let lightbulb3= document.querySelector('#lightbulb3');
 
-// let bulbArray = [lightbulb1,lightbulb2,lightbulb3]
-
 let subtitle = document.querySelector('.subtitle');
 
+let bulbArray = [lightbulb1,lightbulb2,lightbulb3];
+
 let count = 0;
+
+for (const bulb of bulbArray) {
+
+    bulb.addEventListener('click',(e) =>{
+        
+        count++;
+        subtitle.innerHTML = `You've clicked the light ${count} time(s)`;
+        bulb.classList.toggle("active");
+
+    });
+    
+};
 
