27736c2144609c6c651a60a2b14553ae9bf12007
Sione <dev.sionejoseph@gmail.com>
2024-09-04 20:15:48 -0500

    linked script and added on click(s)

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ console.log("script connected successfully!")
 let lightbulb1= document.querySelector('#lightbulb1');
 let lightbulb2= document.querySelector('#lightbulb2');
 let lightbulb3= document.querySelector('#lightbulb3');
+
+// let bulbArray = [lightbulb1,lightbulb2,lightbulb3]
+
+let subtitle = document.querySelector('.subtitle');
+
+let count = 0;
+
